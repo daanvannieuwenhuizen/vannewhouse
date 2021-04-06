b5ccb27e14fb8e28948c9fabc7f92ccd8140fc1d
Daan van Nieuwenhuizen <daan@van-nieuwenhuizen.nl>
2021-04-06 22:25:43 +0200

    Reset client with CRA

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { Counter } from './features/counter/Counter';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <Counter />
-        <h1>
+        <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </h1>
+        </p>
         <span>
           <span>Learn </span>
           <a
