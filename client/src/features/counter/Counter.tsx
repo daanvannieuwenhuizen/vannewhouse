b92ddfc2302ab675ea6e52c4d4343f69df285883
Daan van Nieuwenhuizen <d.vannieuwenhuizen@mechdes.nl>
2021-04-06 16:20:03 +0200

    intial commit

---
+++ client/src/features/counter/Counter.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  incrementAsync,
+  selectCount,
+} from './counterSlice';
+
+export function Counter() {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState('2');
+
+  return (
+    <div>
+     
+    </div>
+  );
+}
